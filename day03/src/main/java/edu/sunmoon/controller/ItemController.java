9f22c700d1efa541b2b7a9292220e2114e49c1d6
yeonkyun <jungyk411@gmail.com>
2024-10-29 13:26:50 +0900

    Refactor Item Search Function

--- day03/src/main/java/edu/sunmoon/controller/ItemController.java
+++ day03/src/main/java/edu/sunmoon/controller/ItemController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
